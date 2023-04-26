6bc203740588e1cd7222d7cc60cdcea26d6867ea
Mr. Ryan <60050719+ryanhhogan3@users.noreply.github.com>
2023-04-25 23:48:44 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask
+import yfinance as yf
+import pandas as pd
+from data import get_OHLC, get_option_dates, get_dividend_hist, get_portfolio, get_news, get_risk
+
+
+app = Flask(__name__)
+
+selected_ticker = "MSFT"
+
+@app.route("/")
+def hello():
+    return 'Hello, World!, Welcome to my portfolio project!'
+
+@app.route("/MSFT")
+def msft_data():
+    html_stock_data = get_OHLC(selected_ticker).to_html()
+    return html_stock_data
+
+@app.route("/options")
+def msft_options():
+    html_opt_data = get_option_dates(selected_ticker).to_html()
+    return html_opt_data
+
+@app.route("/dividend")
+def stock_dividends():
+    html_div_data = get_dividend_hist(selected_ticker).to_html()
+    return html_div_data
+
+@app.route("/portfolio")
+def portfolio_page():
+    html_portfolio = get_portfolio().to_html()
+    return html_portfolio
+
+@app.route("/news")
+def news_page():
+    html_news = get_news().to_html()
+    return html_news
+
+@app.route("/risks")
+def risk_page():
+    html_risks = get_risk().to_html()
+    return html_risks
+
+if __name__ == "__main__":
+    app.run()
---
+++ data.py
@@ -0,0 +1,31 @@
+import yfinance as yf
+import pandas as pd
+
+def get_OHLC(ticker):
+    stockobj = yf.Ticker(ticker)
+    OHLCV = pd.DataFrame(stockobj.history('1y'))
+    return OHLCV
+
+def get_option_dates(ticker):
+    stockobj = yf.Ticker(ticker)
+    option_dates = pd.DataFrame(stockobj.options)
+    return option_dates
+
+def get_dividend_hist(ticker):
+    stockobj = yf.Ticker(ticker)
+    div_hist = pd.DataFrame(stockobj.dividends)
+    return div_hist
+
+def get_portfolio():
+    csv_data = pd.read_csv('CSV_maker - Sheet1.csv')
+    csv_data1 = pd.DataFrame(csv_data)
+    return csv_data1
+
+def get_news():
+    news_data = pd.read_csv("https://raw.githubusercontent.com/suraj-deshmukh/BBC-Dataset-News-Classification/master/dataset/dataset.csv", encoding="ISO-8859-1")
+    return news_data
+
+def get_risk():
+    risk_data = pd.read_csv("https://raw.githubusercontent.com/GFDRR/open-risk-data-dashboard/master/backend/contents/countries/country_groups.csv")
+    return risk_data
+
