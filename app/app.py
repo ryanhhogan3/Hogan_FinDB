2a9d737e9ab3eedfe4032843ac0520fd1b51c079
Mr. Ryan <60050719+ryanhhogan3@users.noreply.github.com>
2023-05-07 22:54:05 -0700

    Added render templates ability to handle dataframe

--- app/app.py
+++ app/app.py
@@ -9,37 +9,38 @@ selected_ticker = "MSFT"
 
 @app.route("/")
 def home():
-    return render_template('index.html', title='Welcome', username='ryan')
+    return render_template('index.html', content="testing")
 
 @app.route("/MSFT")
 def msft_data():
-    html_stock_data = get_OHLC(selected_ticker).to_html()
-    return html_stock_data
+    html_stock_data = get_OHLC(selected_ticker)
+    return render_template('contentWIP.html', tables=[html_stock_data.to_html()], titles=[''])
 
 @app.route("/options")
 def msft_options():
-    html_opt_data = get_option_dates(selected_ticker).to_html()
-    return html_opt_data
+    html_opt_data = get_option_dates(selected_ticker)
+    return render_template('contentWIP.html', tables=[html_opt_data.to_html()], titles=[''])
+
 
 @app.route("/dividend")
 def stock_dividends():
-    html_div_data = get_dividend_hist(selected_ticker).to_html()
-    return html_div_data
+    html_div_data = get_dividend_hist(selected_ticker)
+    return render_template('contentWIP.html', tables=[html_div_data.to_html()], titles=[''])
 
 @app.route("/portfolio")
 def portfolio_page():
-    html_portfolio = get_portfolio().to_html()
-    return html_portfolio
+    html_portfolio = get_portfolio()
+    return render_template('contentWIP.html', tables=[html_portfolio.to_html()], titles=[''])
 
 @app.route("/news")
 def news_page():
-    html_news = get_news().to_html()
-    return html_news
+    html_news = get_news()
+    return render_template('contentWIP.html', tables=[html_news.to_html()], titles=[''])
 
 @app.route("/risks")
 def risk_page():
-    html_risks = get_risk().to_html()
-    return html_risks
+    html_risks = get_risk()
+    return render_template('contentWIP.html', tables=[html_risks.to_html()], titles=[''])
 
 if __name__ == "__main__":
     app.run()
